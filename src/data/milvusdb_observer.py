dc309884746988750d592e542350e2ef94dbdd39
Kourosh Sajjadi <kourosh_sajjadi@outlook.com>
2025-09-10 11:29:27 +0200

    Revert "Upload current RAG code."
    This reverts commit becb43430d89c4a27d7f7e574186c6294eb8209f.

--- src/data/milvusdb_observer.py
+++
@@ -1,7 +0,0 @@
-from pymilvus import connections, utility, Collection
-
-connections.connect(alias="default", host="localhost", port="19530")
-print("Collections:", utility.list_collections())
-
-collection = Collection("gotmat_collection")
-print("Number of entities in collection:", collection.num_entities)
